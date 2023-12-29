5edad2f2283883943dfdda58add3be84713493ed
Marmelade57 <155176356+Marmelade57@users.noreply.github.com>
2023-12-29 17:18:39 +0100

    Update main.c
    Passage au compilateur en ligne

--- main.c
+++ main.c
@@ -5,19 +5,27 @@
 
 struct joueur{
     char nom[51];
-    int points = 0;
+    int points;
     int nb_tentatives;
     int ordre_passage;
-}
+};
 
-int est_joker(*carte){
+int est_joker(int *carte){
     if (*carte = 0){
         return True;
     }
     return False;
+};
+
+void tentatives(int *nb_tentatives, int *points_du_joueur, int *nb_echecs){
+    if (*nb_tentatives = 0){
+        *points_du_joueur -= 1;
+        printf("Vous avez perdu 1 point ce qui vous laisse avec un score de %d", *points_du_joueur);
+        *nb_tentatives = *nb_echecs;
+    };
 }
 
-int paire_ou_non(*points_du_joueur, *carte1, *carte2, *nb_tentatives){
+int paire_ou_non(int *points_du_joueur, int *carte1, int *carte2, int *nb_tentatives){
     if (*carte1 == *carte2){
         *points_du_joueur += 1;
         printf("Vous avez trouvé la paire de %d ! Vous avez désormais %d points !", *carte1, *points_du_joueur);
@@ -25,69 +33,66 @@ int paire_ou_non(*points_du_joueur, *carte1, *carte2, *nb_tentatives){
     else{
         *nb_tentatives -= 1;
         printf("Les cartes avaient des valeurs différentes : %d et %d. Il vous reste %d", *carte1, *carte2, *nb_tentatives);
-        if (*nb_tentatives = 0){
-            *points_du_joueur -= 1;
-            printf("Vous avez perdu 1 point ce qui vous laisse avec un score de %d", *points_du_joueur);
-            *nb_tentatives = *nb_echecs;
-        }
-    }
+        tentatives(&(&nb_tentatives));
+    };
 }
 
-void creation_jeu(*nb_paires){
+void creation_jeu(int *nb_paires){
 
 }
 
-void init_joueurs(*nb_joueurs){
+void init_joueurs(int *nb_joueurs){
     int i;
     while (*nb_joueurs != i){
         printf("Entrez un nom de 50 caractères ou moins pour le joueur :");
-        fgets((joueur.nom), 50, stdin);
+        fgets(&(joueur.nom), 50, stdin);
         joueur.ordre_passage = i;
     }
 }
 
 void initialisation_partie(){
-    int l, c, nb_joueurs, ordinateur=False, joueur_ordinateur;
+    int l, c, nb_joueurs, ordinateur=False, joueur_ordinateur, n;
     printf("Indiquez le nombre de joueurs (maximum 4 et sans compter l'ordinateur) : ");
     scanf("%d", nb_joueurs);
-    if (*nb_joueurs < 1) or (*nb_joueurs >= 5){
+    if ((nb_joueurs < 1) || (nb_joueurs >= 5)){
         printf("Veuillez entrer un nombre valide de joueurs ! ");
     }
-    if (*nb_joueurs >= 1) and (*nb_joueurs <= 3){
+    if ((nb_joueurs >= 1) && (nb_joueurs <= 3)){
         printf("Voulez-vous que l'ordinateur joue avec vous ? (Oui : 1, Non : 2)");
         scanf("%d", &joueur_ordinateur);
-        if (*joueur_ordinateur == 1){
+        if (joueur_ordinateur == 1){
             ordinateur=True;
             printf("D'accord, il y aura un ordinateur alors !");
-            *nb_joueurs += 1;
+            nb_joueurs += 1;
         }
-        else if (*joueur_ordinateur == 2){
+        else if (joueur_ordinateur == 2){
             printf("D'accord, il n'y aura pas d'ordinateur alors !");
         }
         else {
-            printf("La valeur entrée est invalide, réessayez.")
-        }
-    }
+            printf("La valeur entrée est invalide, réessayez.");
+        };
+    };
     printf("Choisissez le nombre de lignes (3, 5 ou 7) et de colonnes (5, 7 ou 9) du plateau. ");
-    scanf("%d %d", *l, *c);
-    while ((*l != 3) or (*l != 5) or (*l != 7)) or ((*c != 5) or (*c != 7) or (*c != 9)) {
-        if ((*l != 3) and (*l != 5) and (*l != 7)){
+    scanf("%d %d", &l, &c);
+    while (l!=3 || l!=5 || l!=7 || c!=5 || c!=7 || c!=9){
+        if ((l != 3) && (l != 5) && (l != 7)){
             printf("Entrez un nombre de lignes valide (3, 5 ou 7) :");
-            scanf("%d", *l);
-        }
-        if ((*c != 5) and (*c != 7) and (*c != 9)){
+            scanf("%d", l);
+        };
+        if ((c != 5) && (c != 7) && (c != 9)){
             printf("Entrez un nombre de colonnes valide (5, 7 ou 9) :");
-            scanf("%d", *c);
-        }
+            scanf("%d", c);
+        };
     }
-    int n = ((l*c) - 1) / 2;
+    n=(((l*c) - 1) / 2);
     creation_jeu(&n);
-    printf("Choisissez le nombre d'échecs maximum : ")
+    int nb_echecs;
+    printf("Choisissez le nombre d'échecs maximum : ");
     scanf("%d", nb_echecs);
     if (nb_echecs < 1){
         printf("Le nombre d'échecs doit être de 1 au minimum. Réessayez.");
         scanf("%d", nb_echecs);
-    }
+    };
 }
 
 void affiche_tableau(){
@@ -97,9 +102,6 @@ void affiche_tableau(){
 int main () {
     
 }
-
-
-
 
 /*
 int paires;
