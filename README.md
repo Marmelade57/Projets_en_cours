1990a63eb930699eab44fc8b6d17abdb46d5642e
Marmelade57 <margaux.haller.mb@gmail.com>
2025-01-21 22:49:52 +0100

    Ajout d'une barre de recherche non fonctionnelle

--- README.md
+++ README.md
@@ -9,4 +9,4 @@ Bienvenue sur ce git où je mets quelques projets non finis que j'ai fait pendan
 Compilation de fichiers fait en <span style="color:red">C</span> pour jouer à un jeu de carte : le mémorix.
 
 ### appli_livre
-Test d'application avec le framework <span style="color:red">Flutter</span> pour apprendre les bases et pouvoir avoir sous la main une liste de tous les bouquins que je possède.
+Test d'application avec le framework <span style="color:red">Flutter</span> pour apprendre les bases pour la <a href="https://github.com/Marmelade57/SAE-semestre-3">SAE 301</a> et pouvoir avoir sous la main une liste de tous les bouquins que je possède.
--- appli_livre/lib/pages/accueil.dart
+++ appli_livre/lib/pages/accueil.dart
@@ -1,3 +1,5 @@
+//import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class PageAccueil extends StatelessWidget {
@@ -11,25 +13,93 @@ class PageAccueil extends StatelessWidget {
         backgroundColor: Theme.of(context).colorScheme.primary,
         foregroundColor: Theme.of(context).colorScheme.surface,
       ),
-      drawer: const Drawer(
-        child: Column(
-          children: [
-          DrawerHeader(child: Icon(
-            Icons.menu_book,
-            size:48,
-          ),),
-          ListTile(
-            leading: Icon(Icons.home_outlined),
-            title: Text("Accueil"),
+      drawer: menu(),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const SizedBox(
+            height: 16,
           ),
-          ListTile(
-            leading: Icon(Icons.favorite_outline),
-            title: Text("Favoris"),
-          )
-          ]
-        ),
+          const Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SearchBar(
+                  hintText: "Rechercher un titre",
+                  //onSubmitted: Navigator.pushNamed(context, "/recherche"),
+                ),
+                Icon(Icons.search),
+              ],
+            ),
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+        ],
       ),
     );
-
   }
+
+  Drawer menu() {
+    return const Drawer(
+      child: Column(children: [
+        DrawerHeader(
+          child: Icon(
+            Icons.menu_book,
+            size: 48,
+          ),
+        ),
+        ListTile(
+          leading: Icon(Icons.home_outlined),
+          title: Text("Accueil"),
+        ),
+        ListTile(
+          leading: Icon(Icons.favorite_outline),
+          title: Text("Favoris"),
+        )
+      ]),
+    );
+  }
+
+  /* Text filtre(BuildContext context){
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Filtrer par type de tags"),
+          content: SizedBox(
+            width: double.maxFinite,
+            height: 300,
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Text(index.toString());
+              },
+              itemCount: 3,
+              /* itemCount: typeTags.length, // Nombre réel de types de tags
+              itemBuilder: (context, index) {
+                final typeTag = typeTags[index]; // Utilise un type réel de la liste
+
+                return ListTile(
+                  title: Text(typeTag.nomComplet), // Affiche le nom complet réel
+                  onTap: () {
+                    Navigator.pop(context);
+                    _showTagSelectionDialog(context, typeTag);
+                  },
+                );
+              }, */
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Annuler", style: TextStyle(color: Colors.red),),
+            ),
+          ],
+        );
+      },
+    );
+  } */
 }
